c2e0c134a51f6458bda3af64cb4b430cf3da2dda
sajan Limbu <limbusajan087@gmail.com>
2023-12-21 22:56:01 +0545

    added

---
+++ 01/index.js
@@ -0,0 +1,12 @@
+function changetheme(){
+
+    document.body.classList.toggle("dark-theme");
+
+    /* if(document.body.classList.contains("dark-theme")){
+        document.body.classList.remove("dark-theme");
+    }
+    else 
+    document.body.classList.add("dark-theme"); */
+    
+}; 
+
